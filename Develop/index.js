0b437949da9d5f6cc207f9eb1d45ad762708f4c9
Amna Syeda <amna.syeda98@gmail.com>
2021-06-14 10:02:04 -0400

    index.js + package.json initial code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./src/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: "input",
+		name: "name",
+		message: "What is your name?",
+	},
+    {
+		type: "input",
+		name: "email",
+		message: "What is your email address?",
+	},
+    {
+		type: "input",
+		name: "username",
+		message: "What is your Github username?",
+	},
+	{
+		type: "input",
+		name: "repository",
+		message: "What is the name of yout GitHub repository?",
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "Please describe your repository.",
+	},
+	{
+		type: "input",
+		name: "installation",
+		message: "What must be installed to run this project?",
+	},
+		];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err, data) => {
+		return err ? err : data;
+	});
+} 
+	
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+};
 
 // Function call to initialize app
 init();
